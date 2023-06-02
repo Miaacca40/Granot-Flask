2e9e800a1554d45c4685df6c43bcc86b78159844
Miaacca40 <135208157+Miaacca40@users.noreply.github.com>
2023-06-02 11:08:05 -0700

    a
    aa

--- app.py
+++ app.py
@@ -9,12 +9,17 @@ def home_page():
 
 @app.route('/market')
 def market_page():
-    items = [
-        {'id': 1, 'name': 'Phone', 'barcode': '893212299897', 'price': 500},
-        {'id': 2, 'name': 'Laptop', 'barcode': '123985473165', 'price': 900},
-        {'id': 3, 'name': 'Keyboard', 'barcode': '231985128446', 'price': 150}
-    ]
+    conn = sqlite3.connect('granot.db')
+    cursor = conn.cursor()
+    cursor.execute('SELECT * FROM inventory')
+    items = cursor.fetchall()
+    conn.close()
     return render_template('market.html', items=items)
+
+@app.route('/estimate')
+def estimate():
+    return render_template('estimate.html')
+
 @app.route('/create_table')
 def create_table():
     # Database connection
